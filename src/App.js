0efc4a91dbd9c1e1c51f4f7085d7ff882e3f2492
xmodzbot <xmodzbot@gmail.com>
2023-09-30 00:08:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+import CardDetails from './components/CardDetails'
+import Cards from './components/Cards'
+import {Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Cards/>}   />
+        <Route path='/cart/:id' element={<CardDetails/>}   />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
